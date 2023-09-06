46ed764816965fa30276dd65c9f40a5b1a42a5ac
molo06002 <molo06002@E-PW0234KQ>
2023-09-06 12:47:49 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace uppgift_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("Fatal Error!");
+            Console.ReadLine();
+        }
+    }
+}
